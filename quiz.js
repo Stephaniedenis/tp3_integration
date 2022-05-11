2643f12da77cd46f2220c49a92f2c80a7e526f57
Amy Bienvenu <amy.bienvenu@outlook.com>
2022-05-11 18:45:56 -0400

    quiz et debut sommaire

---
+++ quiz.js
@@ -0,0 +1,189 @@
+var JSONdata = `
+    {
+        "questionnaire" : [
+        {
+            "question":"Vrai ou faux ? La phase de conception permet de spécifier dans les grandes lignes comment l'application Web doit se comporter tel que perçu par l'utilisateur",
+            "choix": [
+                "Vrai",
+                "Faux"
+            ],
+            "reponse": 1
+        },
+        {
+            "question":"Que signifie « modéliser » un logiciel",
+            "choix":[
+                "Réalisation des tests de validation d’un logiciel",
+                "Représentation à l’aide de différents diagrammes",
+                "Écriture de l’algorithme d’un logiciel",
+                "Modification d’un logiciel existant",
+                "Toutes ces réponses"],
+            "reponse": 2
+        },
+        {
+            "question":"Qu'est ce que le contexte d'un système ou logiciel",
+            "choix": ["Les systèmes en relation avec le système","Les relations du logiciel","C’est l’environnement direct du logiciel","Les cas d'utilisation du système","Aucune de ces réponses"],
+            "reponse": 3
+        },
+        {
+            "question":"Quel est la relation entre deux cas d'utilisation qui indique que le second cas d'utilisation est toujours exécuté",
+            "choix": [
+                "Exclude",
+                "Include",
+                "Extend",
+                "Association",
+                "Aucune de ces réponses"
+            ],
+            "reponse": 2
+        },
+        {
+            "question":"Qui suis-je ? Je suis considéré comme un référentiel partagé par un prestataire de service et l’équipe interne.",
+            "choix": [
+                "Cahier de production",
+                "Diagramme de cas d'utilisation",
+                "UML",
+                "Cahier des charges",
+                "Aucune de ces réponses"
+            ],
+            "reponse": 4
+        },
+        {
+            "question":"Qui suis-je ? Une collection d’activité pour produire un résultat pour un client ou un marché.",
+            "choix":[
+                "Un cas d'utilisation",
+                "Diagramme de classe",
+                "Business Process Modeling Notation",
+                "Processus métier",
+                "Aucune de ces réponses"
+            ],
+            "reponse": 4
+        },
+        {
+            "question":"Qu'est ce que UML",
+            "choix": [
+                "Un langage de modélisation",
+                "Un langage de développement objet",
+                "Une démarche de définition de logiciel",
+                "Une méthode de gestion de projet",
+                "Aucune de ces réponses"
+            ],
+            "reponse": 1
+        },
+        {
+            "question" : "De combien de diagrammes se compose UML",
+            "choix" : [
+                "4 + 1 diagrammes",
+                "9 diagrammes",
+                "Une vingtaine de diagrammes",
+                "14 diagrammes",
+                "Aucune des ces réponses"
+            ],
+            "reponse" : 4
+        } 
+    ]
+}`;
+
+var self;
+var callback;
+
+class Quiz {
+    constructor (progressbar,pointage,total,no,question,choix) 
+    {
+        this.HTMLprogressbar = progressbar;
+        this.HTMLpointage = pointage;
+        this.HTMLtotal = total;
+        this.HTMLnumero = no;
+        this.HTMLquestion = question;
+        this.HTMLchoix = choix;
+        self = this;
+    }
+    startQuiz(callBack) {
+        callback = callBack;
+        this.numero = -1;
+        this.points = 0;
+        this.tentative = JSON.parse(JSONdata);
+        this.total = this.tentative.questionnaire.length;
+        this.HTMLtotal.text(this.total);
+        this.updatePointage();
+        this.loadNextQuestion();
+    }
+    updateProgressBar() {
+        this.HTMLprogressbar.progressbar({"value":this.numero,"max":this.total}).addClass('show');
+    }
+    updatePointage() {
+        this.HTMLpointage.text(this.points);
+    }
+    loadNextQuestion() {
+        this.numero++;
+        this.updateProgressBar();
+        if (this.numero<this.total) {
+            this.HTMLnumero.text((this.numero+1)+".");
+            this.HTMLquestion.text(this.tentative.questionnaire[this.numero].question);
+    
+            let choix = this.tentative.questionnaire[this.numero].choix;
+            let reponse = this.tentative.questionnaire[this.numero].reponse;
+    
+            this.HTMLchoix.html("");
+    
+            let i;
+            for (i=0;i<choix.length;i++) {
+                let div = $(`<div class="choix light" id="${i+1}">${choix[i]}</div>`);
+                if (reponse == i+1) {    
+                    div.on('click',this.bonChoix);
+                } else {
+                    div.on('click',this.mauvaisChoix);
+                }
+                this.HTMLchoix.append(div);
+            }
+            this.clickable = true;
+        } else {
+            alert("FIN DU QUIZ");
+            callback();
+        }
+    }
+    bonChoix(e){
+        if (self.clickable)
+        {
+            e.currentTarget.classList.add("good");
+            self.removeLightsEffect();                        
+            self.points++;
+            self.updatePointage();
+            self.clickable = false;
+            self.tentative.questionnaire[self.numero].reponseChoisie = parseInt(e.target.id);
+        } else {
+            self.loadNextQuestion();
+        }
+    }
+    mauvaisChoix(e) {
+        if(self.clickable) {
+            e.currentTarget.classList.add("bad");
+            self.reveal();
+            self.clickable = false;
+            self.tentative.questionnaire[self.numero].reponseChoisie = parseInt(e.target.id);
+        } else {
+            self.loadNextQuestion();
+        }
+    }
+
+    removeLightsEffect() {
+        let i;
+        let choix;
+        for (i=0;i<this.tentative.questionnaire[this.numero].choix.length;i++) {
+            $(`#${i+1}`).removeClass("light");
+        }
+    }
+    reveal() {
+        $(`#${this.tentative.questionnaire[this.numero].reponse}`).addClass("good");
+        this.removeLightsEffect();
+    }
+}
+
+
+    
+// }
+
+// function pad(n, width, z) {
+//     z = z || '0';
+//     n = n + '';
+//     return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+// }
+
--- script.js
+++ script.js
@@ -1,43 +1,18 @@
 // 'use strict'
-let profil;
+let profil = {
+    "nom" : "Bienvenu",
+    "prenom" : "Amy",
+    "date" : "1975-06-18",
+    "occupation" : "Étudiant"
+};
 let estSoumis = false;
 let etat = "chargement";
-
-const HTMLnumero = document.getElementById("numero");
-const HTMLpoints = document.getElementById("points");
-const HTMLtotal = document.getElementById("total");
-const HTMLquestion = document.getElementById("question");
-const HTMLchoix = document.getElementById("choix");
-const HTMLminutes = document.getElementById("minutes");
-const HTMLsecondes = document.getElementById("secondes");
-
-// $(function () {
-//     $(".contenu").hide()
-//     $(".titre").on("click", function () {
-//         $(".contenu").hide(1000)
-//         $(this).next().show(1000)
-//     })
-// })
-// $(function () {
-//     $(".contenu").hide();
-//     $(".titre").click;
-//     $(".titre").on("click", function () {
-//         $(".contenu").hide(1000);
-       
-//         $( ".inner" ).after( "<p>Test</p>" );
-//         $( "<p>Test</p>" ).insertAfter( ".inner" );
-       
-//         $(this).parent().find(".contenu").show(1000);
-//         $(this).append(`<p>PHILIPPE</p>`);
-        
-//     });
-// });
-
-// clearMessage();
+let startingState = "quiz";
+let quiz;
 
 jQuery(function() {
     clearMessage();
-    changeEtat("formulaire");
+    changeEtat(startingState);
 });
 
 function changeEtat(nom) {
@@ -49,7 +24,6 @@ function changeEtat(nom) {
         $(this).removeClass('show');
     });
     $('#'+nom).addClass('show');
-
     // addMessage(`${etat}`);
 
     switch (etat) {
@@ -110,7 +84,7 @@ function lancerFormulaire() {
                     "date" : date.value,
                     "occupation" : occupation.value
                 };
-                changeEtat("quiz");
+                changeEtat("sommaire");
             }, 
             showErrors: function (errorMap, errorList) {
                 clearMessage();
@@ -146,12 +120,26 @@ function lancerFormulaire() {
 }
 
 function lancerQuiz() {
-    // addMessage(`Bonjour ${JSON.stringify(profil)}`);
-    startQuiz();   
+    quiz = new Quiz($('#progressbar'),$('#points'),$('#total'),$('#numero'),$('#question'),$('#choix')); 
+    quiz.startQuiz(lancerModal);
+}
+
+function lancerModal() {
+    $('#modal').addClass('show');
+    changeEtat('sommaire');
+    // lancer modal
+}
+
+function age(naissance) {
+    naissance = new Date(naissance);
+    var aujourdhui = new Date();
+    return Math.floor((aujourdhui-naissance) / (365.25 * 24 * 60 * 60 * 1000));
 }
 
 function lancerSommaire() {
-    // sommaire();    
+    // Remplir Écran Sommaire avec les informations reçues.
+    remplirSommaireProfil();
+    remplirSommaireAccordeon();
 }
 
 function clearMessage() {
@@ -163,213 +151,23 @@ function addMessage(message) {
     $("#afficherErreurs").children('ul').append(`<li>${message}</li>`)
 }
 
-var points = 0;
-var numero = 0;
-var total = 0; 
-var temps = 0; // temps en secondes
-
-var horloge;
-
-var clickable = true;
-var quiz = true;
-
-function pad(n, width, z) {
-    z = z || '0';
-    n = n + '';
-    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+function remplirSommaireProfil() {
+    $('#sommaire1').append(`<p>Prénom : ${profil.prenom}</p>`)
+    $('#sommaire1').append(`<p>Nom : ${profil.nom}</p>`)
+    $('#sommaire1').append(`<p>Âge : ${age(profil.date)}</p>`)
+    $('#sommaire1').append(`<p>Statut : ${profil.occupation}.e</p>`);
 }
 
-function updateClock() {
-    HTMLminutes.innerHTML = Math.floor(temps/60);
-    HTMLsecondes.innerHTML = pad(temps % 60,2);
-}
-
-function startQuiz() {
-    quiz = true;
-    numero = -1;
-    points = 0;
-    total = data.length;
-    temps = 600;
-    updateClock();
-    horloge = setInterval(()=> {
-        temps--;
-        updateClock();
-        if (temps == 0) {
-            quiz = false;
-            clickable = false;
-            stopClock();
-            alert("Temps Écoulé");
-        }
-    }, 1000);
-    HTMLtotal.innerHTML = total;
-    loadNextQuestion();
-}
-
-
-function stopClock() {
-    clearInterval(horloge);
-}
-
-function loadNextQuestion() {
-    numero++;
-    if (numero<total) {
-        HTMLnumero.innerHTML = (numero+1)+".";
-        HTMLpoints.innerHTML = points;
-        HTMLquestion.innerHTML = data[numero].question;
-
-        let choix = data[numero].choix;
-        let reponse = data[numero].reponse;
-
-        HTMLchoix.innerHTML = "";
-
-        let i;
-        for (i=0;i<choix.length;i++) {
-            let div = document.createElement("div");
-            div.id = "choix"+(i+1);
-            div.innerHTML = choix[i];
-            div.classList.add("choix");
-            div.classList.add("light");
-            if (reponse == i+1) {    
-                div.addEventListener("click", (e) => {
-                    if (clickable && quiz)
-                    {
-                        e.currentTarget.classList.add("good");
-                        removeLightsEffect();                        
-                        points++;
-                        HTMLpoints.innerHTML = points;
-                        clickable = false;
-                    } else {
-                        if (quiz) loadNextQuestion();
-                    }
-                });
-            } else {
-                div.addEventListener("click", (e) => {
-                    if (clickable && quiz)
-                    {
-                        e.currentTarget.classList.add("bad");
-                        reveal();
-                        clickable = false;
-                    } else {
-                        if (quiz) loadNextQuestion();
-                    }
-                });
-            }
-            HTMLchoix.appendChild(div);
-        }
-
-        clickable = true;
-    } else {
-        stopClock();
-        alert("Quiz Terminé");
+function remplirSommaireAccordeon() {
+    for (i=0;i<quiz.total;i++) {
+        $('#accordion').append(
+            `<h3>Question ${i+1}</h3>
+            <div>
+            <p>${quiz.tentative.questionnaire[i].question}</p>
+            <p>Vous avez répondu : ${quiz.tentative.questionnaire[i].choix[quiz.tentative.questionnaire[i].reponseChoisie-1]}</p>
+            </div>
+            `);
     }
+    $("#accordion").accordion();
 }
 
-function removeLightsEffect() {
-    let i;
-    let choix;
-    for (i=0;i<data[numero].choix.length;i++) {
-        choix = document.getElementById("choix"+(i+1));
-        choix.classList.remove("light");
-    }
-}
-
-function reveal() {
-    let bonChoix = document.getElementById("choix"+(data[numero].reponse));
-    bonChoix.classList.add("good");
-    removeLightsEffect();
-}
-
-
-  var data = [
-    {
-        "question":"Vrai ou faux ? La phase de conception permet de spécifier dans les grandes lignes comment l'application Web doit se comporter tel que perçu par l'utilisateur",
-        "choix": [
-            "Vrai",
-            "Faux"
-        ],
-        "reponse": 1
-    },
-    {
-        "question":"Que signifie « modéliser » un logiciel",
-        "choix":[
-            "Réalisation des tests de validation d’un logiciel",
-            "Représentation à l’aide de différents diagrammes",
-            "Écriture de l’algorithme d’un logiciel",
-            "Modification d’un logiciel existant",
-            "Toutes ces réponses"],
-        "reponse": 2
-    },
-    {
-        "question":"Qu'est ce que le contexte d'un système ou logiciel",
-        "choix": ["Les systèmes en relation avec le système","Les relations du logiciel","C’est l’environnement direct du logiciel","Les cas d'utilisation du système","Aucune de ces réponses"],
-        "reponse": 3
-    },
-    {
-        "question":"Quel est la relation entre deux cas d'utilisation qui indique que le second cas d'utilisation est toujours exécuté",
-        "choix": [
-            "Exclude",
-            "Include",
-            "Extend",
-            "Association",
-            "Aucune de ces réponses"
-        ],
-        "reponse": 2
-    },
-    {
-        "question":"Qui suis-je ? Je suis considéré comme un référentiel partagé par un prestataire de service et l’équipe interne.",
-        "choix": [
-            "Cahier de production",
-            "Diagramme de cas d'utilisation",
-            "UML",
-            "Cahier des charges",
-            "Aucune de ces réponses"
-        ],
-        "reponse": 4
-    },
-    {
-        "question":"Qui suis-je ? Une collection d’activité pour produire un résultat pour un client ou un marché.",
-        "choix":[
-            "Un cas d'utilisation",
-            "Diagramme de classe",
-            "Business Process Modeling Notation",
-            "Processus métier",
-            "Aucune de ces réponses"
-        ],
-        "reponse": 4
-    },
-    {
-        "question":"Qu'est ce que UML",
-        "choix": [
-            "Un langage de modélisation",
-            "Un langage de développement objet",
-            "Une démarche de définition de logiciel",
-            "Une méthode de gestion de projet",
-            "Aucune de ces réponses"
-        ],
-        "reponse": 1
-    },
-    {
-        "question" : "De combien de diagrammes se compose UML",
-        "choix" : [
-            "4 + 1 diagrammes",
-            "9 diagrammes",
-            "Une vingtaine de diagrammes",
-            "14 diagrammes",
-            "Aucune des ces réponses"
-        ],
-        "reponse" : 4
-    },
-  
-];
-
-$( function() {
-    $( "#accordion" ).accordion();
-    
-
-  } );
-  $( function() {
-    $( "#progressbar" ).progressbar({
-      value: 10
-    });
-  } );
- 
